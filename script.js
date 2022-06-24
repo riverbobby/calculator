cf4773a64d907f28dca084d257a59bdaa64f2dd5
Justin Townley <justin.d.townley@gmail.com>
2022-06-23 17:18:30 -0700

    Add Basic Functionality in js.
    Still need to make sure result does not overflow display.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//global variables
+let tempNum = "";
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+//select elements
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clear = document.querySelector('#clear');
+const point = document.querySelector('#point');
+const equals = document.querySelector('#equals');
+disabled(equals);
+
+//add event handlers
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', (e) => clickedNumber(e.target));
+}
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', (e) => clickedOperator(e.target));
+}
+equals.addEventListener('click', clickedEquals);
+clear.addEventListener('click', () => window.location.reload()); 
+
 function operate(a, b, operator) {
     let result = "";
 
@@ -5,14 +29,12 @@ function operate(a, b, operator) {
         result = add(a, b);
     }else if (operator === '-') {
         result = subtract(a, b);
-    }else if (operator === '*') {
+    }else if (operator === 'x') {
         result = multiply(a, b);
-    }else if (operator === '/') {
-        result = divide(a, b);
     }else {
-        result = "Unknown ERROR";
+        result = divide(a, b);
     }
-
+    refreshDisplay(result);
 }
 
 function add(a, b) {
@@ -29,12 +51,55 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        return "Divide by zero ERROR";
+        return "ERROR";
     }else {
         return a / b;
     }
 }
 
-function refreshDisplay() {
+function refreshDisplay(show) {
+    const display = document.querySelector('#display');
+    display.textContent = show;
+}
 
+function clickedNumber(button) {
+    if (button.id === "point") {
+        disabled(button);
+    }
+    tempNum += button.textContent;
+    refreshDisplay(tempNum);
+    if (tempNum.length > 9 || tempNum.length > 1 && tempNum[tempNum.length-2] === '.'){
+        numbers.forEach(disabled);
+    }
+    if (tempNum !== "" && operator !== "" && button.id !== "point") {
+        enabled(equals);
+    }
+}
+
+function clickedOperator(button) {
+    num1 = +tempNum;
+    tempNum = "";
+    operator = button.textContent;
+    refreshDisplay(operator);
+    numbers.forEach(enabled);
+    operators.forEach(disabled);
+}
+
+function clickedEquals(button) {
+    num2 = +tempNum;
+    operate(num1, num2, operator);
+}
+
+function disabled(button) {
+    button.disabled = true;
+}
+
+function enabled(button) {
+    button.disabled = false;
+}
+
+function disableAll() {
+    numbers.forEach(disabled);
+    operators.forEach(disabled);
+    disabled(equals);
 }
